60a61811ccde900bd4e1e1176692f4c3d4cc272d
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2021-05-30 12:17:43 +0200

    Docker progress

--- command-line/docker/blender_install_vrm_addon.py
+++ command-line/docker/blender_install_vrm_addon.py
@@ -3,3 +3,4 @@ import bpy
 
 bpy.ops.preferences.addon_install (overwrite=True, target='DEFAULT', filepath=os.getcwd() + 'VRM_Addon_for_Blender-release.zip')
 bpy.ops.preferences.addon_enable(module = 'VRM_Addon_for_Blender-release')
+bpy.ops.wm.save_userpref()
